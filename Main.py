659270fd45b493d5b0e360a6a986e6c40f031892
Raghav Atris <atrisraghav@gmail.com>
2023-04-03 20:54:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,65 +1,90 @@
-import cv2
+import cv2 
 import mediapipe as mp
-from math import hypot
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import numpy as np
+import pyautogui
+import time
 
-cap = cv2.VideoCapture(0) #Checks for camera
- 
-mpHands = mp.solutions.hands #detects hand/finger
-hands = mpHands.Hands()   #complete the initialization configuration of hands
-mpDraw = mp.solutions.drawing_utils
+def count_fingers(lst):
+    cnt = 0
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-volbar=400
-volper=0
- 
-volMin,volMax = volume.GetVolumeRange()[:2]
- 
+    thresh = (lst.landmark[0].y*100 - lst.landmark[9].y*100)/2
+
+    if (lst.landmark[5].y*100 - lst.landmark[8].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[9].y*100 - lst.landmark[12].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[13].y*100 - lst.landmark[16].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[17].y*100 - lst.landmark[20].y*100) > thresh:
+        cnt += 1
+
+    if (lst.landmark[5].x*100 - lst.landmark[4].x*100) > 6:
+        cnt += 1
+
+
+    return cnt 
+
+cap = cv2.VideoCapture(0)
+
+drawing = mp.solutions.drawing_utils
+hands = mp.solutions.hands
+hand_obj = hands.Hands(max_num_hands=1)
+
+
+start_init = False 
+
+prev = -1
+
 while True:
-    success,img = cap.read() #If camera works capture an image
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #Convert to rgb
-    
-    results = hands.process(imgRGB) #completes the image processing.
- 
-    lmList = [] #empty list
-    if results.multi_hand_landmarks: #list of all hands detected.
-        #By accessing the list, we can get the information of each hand's corresponding flag bit
-        for handlandmark in results.multi_hand_landmarks:
-            for id,lm in enumerate(handlandmark.landmark): #adding counter and returning it
-                # Get finger joint points
-                h,w,_ = img.shape
-                cx,cy = int(lm.x*w),int(lm.y*h)
-                lmList.append([id,cx,cy]) #adding to the empty list 'lmList'
-            mpDraw.draw_landmarks(img,handlandmark,mpHands.HAND_CONNECTIONS)
-    
-    if lmList != []:
-        x1,y1 = lmList[4][1],lmList[4][2]  #thumb
-        x2,y2 = lmList[8][1],lmList[8][2]  #index finger
-        #creating circle at the tips of thumb and index finger
-        cv2.circle(img,(x1,y1),13,(255,0,0),cv2.FILLED) #image #fingers #radius #rgb
-        cv2.circle(img,(x2,y2),13,(255,0,0),cv2.FILLED) #image #fingers #radius #rgb
-        cv2.line(img,(x1,y1),(x2,y2),(255,0,0),3)  #create a line b/w tips of index finger and thumb
- 
-        length = hypot(x2-x1,y2-y1) #distance b/w tips using hypotenuse
-        vol = np.interp(length,[30,350],[volMin,volMax]) 
-        volbar=np.interp(length,[30,350],[400,150])
-        volper=np.interp(length,[30,350],[0,100])
-        
-        print(vol,int(length))
-        volume.SetMasterVolumeLevel(vol, None)
+    end_time = time.time()
+    _, frm = cap.read()
+    frm = cv2.flip(frm, 1)
 
-        cv2.rectangle(img,(50,150),(85,400),(0,0,255),4) # vid ,initial position ,ending position ,rgb ,thickness
-        cv2.rectangle(img,(50,int(volbar)),(85,400),(0,0,255),cv2.FILLED)
-        cv2.putText(img,f"{int(volper)}%",(10,40),cv2.FONT_ITALIC,1,(0, 255, 98),3)
-    cv2.imshow('Gesture Based Volume Control',img) #Show the video 
-    if cv2.waitKey(1) & 0xff==ord(' '): #By using spacebar delay will stop
+    res = hand_obj.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+    if res.multi_hand_landmarks:
+
+        hand_keyPoints = res.multi_hand_landmarks[0]
+
+        cnt = count_fingers(hand_keyPoints)
+
+        if not(prev==cnt):
+            if not(start_init):
+                start_time = time.time()
+                start_init = True
+
+            elif (end_time-start_time) > 0.2:
+                if (cnt == 1):
+                    pyautogui.press("right")
+                
+                elif (cnt == 2):
+                    pyautogui.press("left")
+
+                elif (cnt == 3):
+                    pyautogui.press("up")
+
+                elif (cnt == 4):
+                    pyautogui.press("down")
+
+                elif (cnt == 5):
+                    pyautogui.press("space")
+                
+                elif (cnt == 6):
+                    pyautogui.press("windows+r")
+ 
+                prev = cnt
+                start_init = False
+
+
+
+
+        drawing.draw_landmarks(frm, hand_keyPoints, hands.HAND_CONNECTIONS)
+
+    cv2.imshow("window", frm)
+
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        cap.release()
         break
-        
-cap.release()     #stop cam       
-cv2.destroyAllWindows() #close window
-'''CLICK SPACE BAR IN ORDER TO STOP THE PROGRAM'''
