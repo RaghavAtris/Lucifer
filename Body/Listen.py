02dc0a0aa264e9203bc227d15b69c474389fe1ee
Raghav Atris <atrisraghav@gmail.com>
2023-04-24 14:56:22 +0530

    Update Listen.py

--- Body/Listen.py
+++ Body/Listen.py
@@ -2,19 +2,14 @@ import speech_recognition as sr #pip install speechrecognition
 from googletrans import Translator #pip install googletrans==3.1.0a0
 
 def Listen(self):
-    
     r = sr.Recognizer()
-    
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source,0,7) # Listening Mode.....
-    
+        audio = r.listen(source,0,5) # Listening Mode.....
+
     try:
         print("Recognizing...")
         query = r.recognize_google(audio,language="hi")
-
     except:
         return ""
     
